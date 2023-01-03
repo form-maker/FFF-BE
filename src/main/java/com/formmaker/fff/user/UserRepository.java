6313c9f296d2598cd4822c6596c4a8a27ab95258
S_0ww <seok6086@kakao.com>
2023-01-03 21:18:59 +0900

    feat: 로그인 아이디 중복확인 구현

--- src/main/java/com/formmaker/fff/user/UserRepository.java
+++ src/main/java/com/formmaker/fff/user/UserRepository.java
@@ -2,7 +2,10 @@ package com.formmaker.fff.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByLoginId(String loginId);
 }
