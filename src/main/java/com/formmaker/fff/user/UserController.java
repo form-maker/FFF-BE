315890c10ffb76aac38f69324390276b8293f198
S_0ww <114342129+seok6086@users.noreply.github.com>
2023-01-04 10:00:53 +0900

    ✨Feat: 로그인아이디 중복확인 구현 (#7)
    * feat: 기본 회원가입 구현
    
    * feat: 로그인 아이디 중복확인 구현

--- src/main/java/com/formmaker/fff/user/UserController.java
+++ src/main/java/com/formmaker/fff/user/UserController.java
@@ -6,7 +6,9 @@ import com.formmaker.fff.user.request.UserSignupRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ResponseMessage> signup(UserSignupRequest userSignupRequest){
+    public ResponseEntity<ResponseMessage> signup(@RequestBody UserSignupRequest userSignupRequest){
         userService.signup(userSignupRequest);
         return new ResponseEntity<>(new ResponseMessage<>("회원가입이 완료되었습니다.",200,null), HttpStatus.OK);
+    }
+    @PostMapping("/signup/loginid")
+    public ResponseEntity<ResponseMessage> duplicateId(@RequestBody UserSignupRequest userSignupRequest){
+        userService.duplicateLoginId(userSignupRequest);
+        return new ResponseEntity<>(new ResponseMessage<>("사용가능한 아이디입니다.",200,null), HttpStatus.OK);
     }
 }
--- src/main/java/com/formmaker/fff/user/UserService.java
+++ src/main/java/com/formmaker/fff/user/UserService.java
@@ -1,10 +1,15 @@
 package com.formmaker.fff.user;
 
-import com.formmaker.fff.common.response.ResponseMessage;
+import com.formmaker.fff.common.exception.CustomException;
+import com.formmaker.fff.common.exception.ErrorCode;
 import com.formmaker.fff.user.request.UserSignupRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.formmaker.fff.common.exception.ErrorCode.DUPLICATE_ID;
 
 @RequiredArgsConstructor
 @Service
@@ -17,4 +22,16 @@ public class UserService {
     public void signup(UserSignupRequest userSignupRequest) {
         this.userRepository.save(userSignupRequest.toUser());
     }
+    /* 아이디,이메일,유저네임 중복확인 */
+    private boolean isDuplicateLoginId(String loginId) {
+        Optional<User> userOptional = userRepository.findByLoginId(loginId);
+        return  userOptional.isPresent();
+
+    }
+    public void duplicateLoginId(UserSignupRequest userSignupRequest){
+        if (isDuplicateLoginId(userSignupRequest.getLoginId())) {
+            throw new CustomException(ErrorCode.DUPLICATE_ID);
+        }
+    }
 }
+
