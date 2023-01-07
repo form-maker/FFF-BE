a57dddf723246604c330cd8a0b2d99d10f3ca13d
faulty337 <37091532+faulty337@users.noreply.github.com>
2023-01-07 20:25:47 +0900

    Feat : 카카오 로그인 구현 (#42)

--- src/main/java/com/formmaker/fff/user/User.java
+++ src/main/java/com/formmaker/fff/user/User.java
@@ -1,13 +1,17 @@
 package com.formmaker.fff.user;
 
 
+import com.formmaker.fff.common.type.SocialTypeEnum;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "users")
 public class User {
@@ -24,10 +28,34 @@ public class User {
 
     private String email;
 
+    @Enumerated(value = EnumType.STRING)
+    private SocialTypeEnum socialType;
+
     public User(String loginId, String username, String password, String email) {
         this.loginId = loginId;
         this.username = username;
         this.password = password;
         this.email = email;
+    }
+
+    public User(String loginId, String username, String email){
+        this.loginId = loginId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public void socialUpdate(SocialTypeEnum type){
+        this.socialType = type;
+    }
+
+    public User(String loginId, String username, String email, SocialTypeEnum socialType) {
+        this.loginId = loginId;
+        this.username = username;
+        this.email = email;
+        this.socialType = socialType;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
