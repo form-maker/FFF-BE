16356394913f0d06ab19f243e3f7cab341f8e67a
S_0ww <114342129+seok6086@users.noreply.github.com>
2023-01-09 09:58:45 +0900

    feat: 마이페이지 조회 기능 (#43)

---
+++ src/main/java/com/formmaker/fff/survey/response/SurveyMyResponse.java
@@ -0,0 +1,20 @@
+package com.formmaker.fff.survey.response;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class SurveyMyResponse {
+
+    private Long surveyId;
+    private String title;
+    private LocalDate deadLine;
+    private LocalDateTime createAt;
+}
+
