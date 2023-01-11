3ed3100375bda6b3b74de33e9ef7af30dee29d5b
S_0ww <seok6086@kakao.com>
2023-01-11 18:32:12 +0900

    refactor: 삭제 API 쿼리 개선

--- src/main/java/com/formmaker/fff/question/repository/QuestionRepository.java
+++ src/main/java/com/formmaker/fff/question/repository/QuestionRepository.java
@@ -2,9 +2,15 @@ package com.formmaker.fff.question.repository;
 
 import com.formmaker.fff.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    void deleteAllBySurveyId(Long surveyId);
 
-    Question findAllById(Long questionId);
+    @Transactional
+    @Modifying
+    @Query("delete from Question c where c.surveyId in :ids")
+    void deleteAllBySurveyId(@Param ("ids")Long ids);
 }
