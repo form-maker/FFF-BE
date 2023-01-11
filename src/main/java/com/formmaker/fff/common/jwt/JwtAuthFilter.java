d30d112bffa123d4b616c93e27e2086d40f8bfb4
faulty337 <faulty337@gmail.com>
2023-01-11 11:26:51 +0900

    Refactor : Security 수정

--- src/main/java/com/formmaker/fff/common/jwt/JwtAuthFilter.java
+++ src/main/java/com/formmaker/fff/common/jwt/JwtAuthFilter.java
@@ -1,11 +1,11 @@
 package com.formmaker.fff.common.jwt;
 
 
-import com.formmaker.fff.common.jwt.exception.CustomSecurityException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.formmaker.fff.common.response.ResponseMessage;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,24 +24,24 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, CustomSecurityException {
-
-
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
         String token = jwtUtil.resolveToken(request, JwtUtil.AUTHORIZATION_HEADER);
 
-        /* Token 유효성 검사 및 인증 */
-        if (token == null) {
-            filterChain.doFilter(request, response);
+        if(request.getRequestURI().contains("api/user")){
+            filterChain.doFilter(request,response);
             return;
         }
-
-        jwtUtil.validateToken(token, request, response);
-
-
-        Claims info = jwtUtil.getUserInfoFromHttpServletRequest(request);
-        setAuthentication(info.getSubject());
-
-        filterChain.doFilter(request, response);
+        System.out.println(request.getRequestURI());
+        /* Token 유효성 검사 및 인증 */
+        if(token != null) {
+            if(!jwtUtil.validateToken(token)){
+                jwtExceptionHandler(response, "로그인 상태를 확인해주세요.", 403);
+                return;
+            }
+            Claims info = jwtUtil.getUserInfo(token);
+            setAuthentication(info.getSubject());
+        }
+        filterChain.doFilter(request,response);
     }
 
     public void setAuthentication(String loginId) {
@@ -52,6 +52,17 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         SecurityContextHolder.setContext(context);
     }
 
+    public void jwtExceptionHandler(HttpServletResponse response, String msg, int statusCode) throws IOException {
+        //클라이언트로 반환하는 부분.
+        response.setStatus(statusCode);
+        response.setContentType("application/json;charset=utf-8");
+        try {
+            String json = new ObjectMapper().writeValueAsString(new ResponseMessage<String>(msg, statusCode, "error"));
+            response.getWriter().write(json);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
 
 
 }
