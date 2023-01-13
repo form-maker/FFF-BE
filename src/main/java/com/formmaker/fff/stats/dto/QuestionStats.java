cd28cbd898a779bbccc6cdf1a84277aaeb9a6d9e
S_0ww <114342129+seok6086@users.noreply.github.com>
2023-01-13 22:04:34 +0900

    Update QuestionStats.java

--- src/main/java/com/formmaker/fff/stats/dto/QuestionStats.java
+++ src/main/java/com/formmaker/fff/stats/dto/QuestionStats.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 public class QuestionStats {
     private Integer questionNum;
-    private String questionType;
+    private QuestionTypeEnum questionType;
 
     private String questionTitle;
     private String questionSummary;
