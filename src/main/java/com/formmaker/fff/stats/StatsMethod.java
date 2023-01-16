e0c86da4b32fb8e9ff4e07a16a03e0fe2b22d631
S_0ww <seok6086@kakao.com>
2023-01-17 02:03:13 +0900

    feat: 설문 통계 조회 Slide API

--- src/main/java/com/formmaker/fff/stats/StatsMethod.java
+++ src/main/java/com/formmaker/fff/stats/StatsMethod.java
@@ -1,16 +1,20 @@
 package com.formmaker.fff.stats;
 
 
-import com.formmaker.fff.answer.dto.AnswerDto;
-import com.formmaker.fff.question.dto.QuestionDto;
-import com.formmaker.fff.reply.dto.ReplyDto;
+import com.formmaker.fff.answer.entity.Answer;
+import com.formmaker.fff.question.entity.Question;
+import com.formmaker.fff.reply.entity.Reply;
 import com.formmaker.fff.stats.dto.QuestionStats;
+import com.formmaker.fff.stats.dto.SatisfactionResponse;
 import com.formmaker.fff.stats.dto.SelectResponse;
-import org.hibernate.sql.SelectValues;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class StatsMethod {
@@ -18,75 +22,135 @@ public class StatsMethod {
     static public StatsMethod statsMethod = new StatsMethod();
 
 
-    public QuestionStats statsSingleChoice(List<ReplyDto> replyDtoList, QuestionDto questionDto){
+    public QuestionStats statsChoice(List<Reply> replyList, Question question) {
 
         List<SelectResponse> selectList = new ArrayList<>();
         SelectResponse selectResponse;
-        for(AnswerDto answerDto : questionDto.getAnswerList()){
-            selectResponse = new SelectResponse(answerDto.getAnswerValue(), answerDto.getAnswerNum());
+        for (Answer answer : question.getAnswerList()) {
+            selectResponse = new SelectResponse(answer.getAnswerValue(), answer.getAnswerNum());
             selectList.add(selectResponse);
         } //해당 질문과 관련된 질문 문항과 문항 번호들을 selectList 담아온다.
 
         selectList = selectList.stream()
-                    .sorted(Comparator.comparing(SelectResponse::getAnswerNum)).collect(Collectors.toList());
+                .sorted(Comparator.comparing(SelectResponse::getAnswerNum)).collect(Collectors.toList());
         //문항 번호별로 정렬해준다.
         int totalSelect = 0;
 
-
-        for(ReplyDto userReply : replyDtoList){
-
-            List<String> selectValueList = List.of(userReply.getSelectValue().split("|"));  //유저들의 문항선택 값을 List에 넣어주고
+        for (Reply userReply : replyList) {
+            List<String> selectValueList = List.of(userReply.getSelectValue().split("\\|"));  //유저들의 문항선택 값을 List에 넣어주고
             List<Integer> changeSelectValueList = selectValueList.stream() //스트링 리스트를 Integer 리스트로 변환
                     .map(Integer::parseInt).toList();
 
 
-
             for (Integer selectValues : changeSelectValueList) { // 유저들의 문항선택 값 리스트 값들을 selectValues 하나하나 넣어주고
-                selectList.get(selectValues-1).increaseValue(); //번호별로 정렬된 문항에서 유저가 문항선택한 값 들이 나올 때마다 해당 문항의 벨류는 증가하고
+                selectList.get(selectValues - 1).increaseValue(); //번호별로 정렬된 문항에서 유저가 문항선택한 값 들이 나올 때마다 해당 문항의 벨류는 증가하고
                 totalSelect++; // 해당 Question의 전체 선택 횟수가 증가한다.
             }
         }
-        for(SelectResponse select : selectList) {
+        for (SelectResponse select : selectList) {
             select.valueAvg(totalSelect); //문항별로 Value= 백분율 통계를 내준다.
         }
 
-       return  QuestionStats.builder()
-                .questionNum(questionDto.getQuestionNum())
-                .questionType(questionDto.getQuestionType())
-                .questionTitle(questionDto.getTitle())
-                .questionSummary(questionDto.getSummary())
+        return QuestionStats.builder()
+                .questionNum(question.getQuestionNum())
+                .questionType(question.getQuestionType())
+                .questionTitle(question.getTitle())
+                .questionSummary(question.getSummary())
                 .selectList(selectList)
                 .build();
-
     }
-    public QuestionStats statsMultipleChoice(List<ReplyDto> replyDtoList, QuestionDto questionDto){
 
-        return QuestionStats.builder().build();
-    }
-    public QuestionStats statsSlide(List<ReplyDto> replyDtoList, QuestionDto questionDto){
 
-        return QuestionStats.builder().build();
+    public QuestionStats statsSlide(List<Reply> replyList, Question question) {
+
+        List<SatisfactionResponse> satisfactionList = new ArrayList<>();
+        SatisfactionResponse satisfactionResponse;
+            int volume = question.getVolume();
+
+
+        for(int x = -volume ; x<=volume; x++){
+           int indexNum = x+volume;
+           int value = indexNum-volume;
+           satisfactionResponse = new SatisfactionResponse(value);
+           satisfactionList.add(satisfactionResponse);
+        }
+        satisfactionList = satisfactionList.stream()
+                .sorted(Comparator.comparing(SatisfactionResponse::getValue)).collect(Collectors.toList());
+
+        int totalSelect = 0 ;
+
+        for (Reply userReply : replyList) {
+            List<String> satisfactionValueList = List.of(userReply.getSelectValue());  //유저들의 선택 값을 List에 넣어주고
+            List<Integer> changeSatisfactionValueList = satisfactionValueList.stream() //스트링 리스트를 Integer 리스트로 변환
+                    .map(Integer::parseInt).toList();
+
+            for (Integer satisfactionValues : changeSatisfactionValueList) { // 유저들의 만족도선택 값 리스트 값들을 satisfactionValues 하나하나 넣어주고
+                satisfactionList.get(satisfactionValues + volume).increaseValue(); //번호별로 정렬된 만족도에서 유저가 만족도를 선택한 값 들이 나올 때마다 해당 문항의 벨류는 증가하고
+                totalSelect++;
+            }
+        }
+        for (SatisfactionResponse satisfaction : satisfactionList) {
+            satisfaction.valueAvg(totalSelect); //문항별로 Value= 백분율 통계를 내준다.
+        }
+        return QuestionStats.builder()
+                .questionNum(question.getQuestionNum())
+                .questionType(question.getQuestionType())
+                .questionTitle(question.getTitle())
+                .questionSummary(question.getSummary())
+                .volume(question.getVolume())
+                .satisfactionList(satisfactionList)
+                .build();
     }
-    public QuestionStats statsRank(List<ReplyDto> replyDtoList, QuestionDto questionDto){
+
+    public QuestionStats statsRank(List<Reply> replyList, Question question) {
+
         //문항 내 폼 1개당 점수 반영 3개면 1명이 3개 반영
         //각 폼에 대해서 합계를 낸 다음에
         //제일 높은 점수를 가진 폼을 필두로 오름차순 , 및 합계치 퍼센테이지로 변환해서 데이터 반환 ?
         return QuestionStats.builder().build();
     }
-    public QuestionStats statsLongDescriptive(List<ReplyDto> replyDtoList, QuestionDto questionDto){
+
+    public QuestionStats statsLongDescriptive(List<Reply> replyList, Question question) {
 
         return QuestionStats.builder().build();
     }
-    public QuestionStats statsShortDescriptive(List<ReplyDto> replyDtoList, QuestionDto questionDto){
+
+    public QuestionStats statsShortDescriptive(List<Reply> replyList, Question question) {
 
         return QuestionStats.builder().build();
     }
-    public QuestionStats statsStar(List<ReplyDto> replyDtoList, QuestionDto questionDto){
 
-        return QuestionStats.builder().build();
+    // 별점, 스코어 통계 처리 로직
+    public QuestionStats statsOfPositiveValue(List<Reply> replyList, Question question) {
+
+
+        return QuestionStats.builder()
+                .questionNum(null)
+                .questionType(null)
+                .questionTitle(null)
+                .questionSummary(null)
+                .questionAvg(null)
+                .satisfactionList(null)
+                .build();
     }
-    public QuestionStats statsScore(List<ReplyDto> replyDtoList, QuestionDto questionDto){
 
-        return QuestionStats.builder().build();
+    /**
+     * String 타입으로 저장된 Json 형식 문자열을 JsonObject
+     * 로 사용할 수 있게 만들어주는 로직
+     * JsonObject 는 HashMap 을 상속받는 클래스이다. Key, Value 로 값에 접근할 수 있다.
+     */
+    private JSONObject toJsonObjectType(String selectValueToStringTypeJsonForm) {
+        try {
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(selectValueToStringTypeJsonForm);
+        } catch (ParseException e) {
+            /**
+             * 에러 메세지는 로그를 통해 남기는 방식을 채택 -> 로그 남기는 방법 알아봐야함.
+             * 에러가 터져서 멈추는게 아닌, 에러가 터진 데이터를 제외한 통계를 반환시켜주어야함.
+             * new CustomException(ErrorCode.INVALID_FORM_DATA);
+             */
+
+        }
+        return new JSONObject();
     }
 }
