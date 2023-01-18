609e660ec34d91a744862fa5b8a1ba42b4491acc
faulty337 <faulty337@gmail.com>
2023-01-18 17:17:24 +0900

    Feat : 날짜별 참가자 수 통계 기능 추가

--- src/main/java/com/formmaker/fff/stats/dto/DailyParticipant.java
+++ src/main/java/com/formmaker/fff/stats/dto/DailyParticipant.java
@@ -1,8 +1,21 @@
 package com.formmaker.fff.stats.dto;
 
-import java.time.LocalDate;
+import lombok.Getter;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class DailyParticipant {
-    private LocalDate date;
-    private Integer participant;
+    private List<LocalDate> date = new ArrayList<>();
+    private List<Integer> participant = new ArrayList<>();
+
+    public void addDate(LocalDate day){
+        this.date.add(day);
+    }
+
+    public void addParticipant(Integer participant){
+        this.participant.add(participant);
+    }
 }
