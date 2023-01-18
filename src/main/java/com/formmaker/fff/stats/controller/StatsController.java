7d19f88776f6c49fb494459332f4f5945bb429b7
faulty337 <37091532+faulty337@users.noreply.github.com>
2023-01-18 11:30:00 +0900

    ✨Feat : 설문 응답 파일 다운로드 기능 추가 (#97)
    * Feat : 설문 응답 파일 다운로드 기능 추가
    
    * Refactor : 빈 응답에 대한 처리

--- src/main/java/com/formmaker/fff/stats/controller/StatsController.java
+++ src/main/java/com/formmaker/fff/stats/controller/StatsController.java
@@ -5,12 +5,18 @@ import com.formmaker.fff.common.response.ResponseMessage;
 import com.formmaker.fff.stats.dto.StatsResponse;
 import com.formmaker.fff.stats.service.StatsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,4 +34,16 @@ public class StatsController {
     //스위치문을 써서 메소드를 가져와서 쓴다?
     //서비스에서 스위치문이랑
     //컨트롤러에서 서비스가는 경로 뚫어주는거랑
+
+
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> downloadCSV(@RequestParam Long surveyId){
+        byte[] csvFile = statsService.getStatsCsvFile(surveyId);
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.valueOf("plain/text"));
+        header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+"test"+".csv");
+        header.setContentLength(csvFile.length);
+        return new ResponseEntity<>(csvFile, header, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/formmaker/fff/stats/service/StatsService.java
+++ src/main/java/com/formmaker/fff/stats/service/StatsService.java
@@ -2,21 +2,39 @@ package com.formmaker.fff.stats.service;
 
 
 import com.formmaker.fff.common.exception.CustomException;
+
+import com.formmaker.fff.common.exception.ErrorCode;
+import com.formmaker.fff.common.type.QuestionTypeEnum;
 import com.formmaker.fff.question.entity.Question;
 import com.formmaker.fff.question.repository.QuestionRepository;
+
 import com.formmaker.fff.reply.entity.Reply;
 import com.formmaker.fff.reply.repository.ReplyRepository;
 import com.formmaker.fff.stats.dto.QuestionStats;
 import com.formmaker.fff.stats.dto.StatsResponse;
 import com.formmaker.fff.survey.entity.Survey;
 import com.formmaker.fff.survey.repository.SurveyRepository;
+import com.formmaker.fff.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static com.formmaker.fff.common.exception.ErrorCode.NOT_FOUND_SURVEY;
 
@@ -48,5 +66,70 @@ public class StatsService {
         }
 
         return StatsResponse.builder().questionStatsList(questionStatsList).build();
+    }
+
+    public byte[] getStatsCsvFile(Long surveyId) {
+        Survey survey = surveyRepository.findById(surveyId).orElseThrow(
+                ()-> new CustomException(NOT_FOUND_SURVEY)
+        );
+
+        List<Question> questionList = survey.getQuestionList();
+
+        byte[] csvFile;
+
+        CSVPrinter csvPrinter;
+        StringWriter sw = new StringWriter();
+
+        String[] headers = Stream.concat(Arrays.stream(new String[]{"유저아이디"}), Arrays.stream(questionList.stream().map(Question::getTitle).toArray(String[]::new))).toArray(String[]::new);
+        Map<User, List<Reply>> userReply = new HashMap<>();
+        for(Question question : questionList){
+            for(Reply reply : question.getReplyList()){
+                List<Reply> replyList = userReply.get(reply.getUser());
+                if(replyList == null){
+                    userReply.put(reply.getUser(), new ArrayList<>(List.of(reply)));
+                }else{
+                    userReply.get(reply.getUser()).add(reply);
+                }
+
+            }
+        }
+
+        try{
+            csvPrinter = new CSVPrinter(sw, CSVFormat.DEFAULT.withHeader(headers));
+            int checkValue;
+            for(User user : userReply.keySet()){
+                List<Reply> replyList = userReply.get(user).stream().sorted(Comparator.comparing(Reply::getQuestionNum)).toList();
+                List<String> userData = new ArrayList<>();
+                String loginId = user.getLoginId();
+                userData.add(loginId.substring(0, loginId.length()-3)+"***");
+                checkValue = 1;
+                for(Reply reply : replyList){
+                    System.out.println(checkValue + " " + reply.getQuestionNum() + " " + reply.getSelectValue());
+                    while (reply.getQuestionNum() > checkValue++){
+                        userData.add("");
+                    }
+                    userData.add(replyToValue(reply));
+                }
+                csvPrinter.printRecord(userData);
+            }
+            sw.flush();
+            csvFile = sw.toString().getBytes("MS949");
+        }catch (IOException e){
+            csvFile = null;
+        }
+
+        return csvFile;
+    }
+
+    public String replyToValue(Reply reply){
+        QuestionTypeEnum type = reply.getQuestionType();
+        switch (type){
+            case MULTIPLE_CHOICE -> {return reply.getSelectValue().replace('|', ',');}
+            case SHORT_DESCRIPTIVE, LONG_DESCRIPTIVE -> {return reply.getDescriptive();}
+            case RANK -> {return reply.getSelectValue().substring(1, reply.getSelectValue().length()-1).replace("\"", "");}
+        }
+        return reply.getSelectValue();
+
+
     }
 }
