025a05e3295e630c1175460b8360ce0e26272b03
faulty337 <faulty337@gmail.com>
2023-01-19 20:37:13 +0900

    Refactor : 설문 상태 업데이트 주기 수정

--- src/main/java/com/formmaker/fff/survey/service/SurveyScheduler.java
+++ src/main/java/com/formmaker/fff/survey/service/SurveyScheduler.java
@@ -16,7 +16,7 @@ public class SurveyScheduler {
 
     private final SurveyRepository surveyRepository;
 
-    @Scheduled(cron = "0 0 * * * ?")
+    @Scheduled(cron = "0 0 0 * * ?")
     public void scheduleTaskUsingCronExpression() {
         LocalDate today = LocalDate.now();
 
