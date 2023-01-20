76d17f5f39363fe9ab9963ae839901ab23aeafd2
faulty337 <faulty337@gmail.com>
2023-01-20 21:44:48 +0900

    Feat : Repository 생성

--- src/main/java/com/formmaker/fff/participant/ParticipantRepository.java
+++ src/main/java/com/formmaker/fff/participant/ParticipantRepository.java
@@ -1,2 +1,6 @@
-package com.formmaker.fff.participant;public class ParticipantRepository {
+package com.formmaker.fff.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 }
