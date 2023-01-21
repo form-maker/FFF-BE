e27622ce24350b88a88f1c45750d90802bfdf92b
faulty337 <faulty337@gmail.com>
2023-01-21 10:32:06 +0900

    Refactor : 날짜별 참가자 통계 기능 수정

--- src/main/java/com/formmaker/fff/participant/ParticipantRepository.java
+++ src/main/java/com/formmaker/fff/participant/ParticipantRepository.java
@@ -1,6 +1,11 @@
 package com.formmaker.fff.participant;
 
+import com.formmaker.fff.survey.entity.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+
+    List<Participant> findAllBySurvey(Survey survey);
 }
