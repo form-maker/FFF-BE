d4e821ca2592c2768ca32805081ac7a10bb0997f
faulty337 <37091532+faulty337@users.noreply.github.com>
2023-01-26 15:52:42 +0900

    ♻️Refactor : sse 객체 유효성 검사 (#154)
    * Refactor : sse 허용
    
    * Refactor : sse 로직 변경
    
    * Refactor : sse 객체 유효성 검사 추가

--- src/main/java/com/formmaker/fff/sse/service/NotificationService.java
+++ src/main/java/com/formmaker/fff/sse/service/NotificationService.java
@@ -2,45 +2,45 @@ package com.formmaker.fff.sse.service;
 
 import com.formmaker.fff.sse.CustomEmitter;
 import com.formmaker.fff.sse.repository.EmitterRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.Cursor;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 @Service
-@RequiredArgsConstructor
 public class NotificationService {
     private static final Long DEFAULT_TIMEOUT = 60L * 1000*2;
 
-    private final RedisTemplate<Long, String> redisTemplate;
-
     private final EmitterRepository emitterRepository;
 
+    private final SetOperations<Long, String> setOperations;
+
+    public NotificationService(RedisTemplate<Long, String> redisTemplate, EmitterRepository emitterRepository) {
+        this.emitterRepository = emitterRepository;
+        setOperations = redisTemplate.opsForSet();
+    }
 
 
     public CustomEmitter joinSurvey(Long surveyId, String sessionId) {
 
-        SetOperations<Long, String> setOperations = redisTemplate.opsForSet();
         if(!setOperations.isMember(surveyId, sessionId)){
             setOperations.add(surveyId, sessionId);
         }
 
         CustomEmitter emitter = emitterRepository.save(new CustomEmitter(new SseEmitter(DEFAULT_TIMEOUT), sessionId));
         setOperations.members(surveyId);
-        notificationAll(setOperations, surveyId);
+
+        notificationAll(surveyId);
+
         emitter.getSseEmitter().onCompletion(() -> {
             emitterRepository.deleteById(sessionId);
             setOperations.remove(surveyId, sessionId);
-            notificationAll(setOperations, surveyId);
+            notificationAll(surveyId);
         });
 
         emitter.getSseEmitter().onTimeout(() -> {
@@ -51,18 +51,17 @@ public class NotificationService {
     }
 
     // 3
-    private void sendToClient(CustomEmitter emitter, Long id, String name, Object data) {
+    private void sendToClient(CustomEmitter emitter, Object data) {
         try {
             emitter.getSseEmitter().send(SseEmitter.event()
-                    .id(id.toString())
-                    .name(name)
                     .data(data));
         } catch (Exception exception) {
             emitterRepository.deleteById(emitter.getId()); //만료된 emitter 제거
         }
     }
 
-    private void notificationAll(SetOperations<Long, String> setOperations, Long surveyId){
+    private void notificationAll(Long surveyId){
+        refreshRedis(surveyId);
         Long size = setOperations.size(surveyId);
         if(size == null || size == 0){
             return;
@@ -79,9 +78,41 @@ public class NotificationService {
                 setOperations.remove(surveyId, id);
                 continue;
             }
-            sendToClient(emitter, surveyId, "join", setOperations.size(surveyId));
+            JsonObject data = new JsonObject();
+            data.addProperty("total", setOperations.size(surveyId));
+            sendToClient(emitter, data.toString());
         }
+    }
 
+    private void refreshRedis(Long surveyId){
+        Long size = setOperations.size(surveyId);
+        if(size == null || size == 0){
+            setOperations.remove(surveyId);
+            return;
+        }
+        Iterator<String> iter = setOperations.members(surveyId).iterator();
+
+        CustomEmitter emitter;
+
+        while(iter.hasNext()){
+            String id = iter.next();
+            emitter = emitterRepository.findByUserId(id);
+            if(emitter == null){
+                setOperations.remove(surveyId, id);
+            }else{
+                if(!connectCheck(emitter)){
+                    emitterRepository.deleteById(id);
+                }
+            }
+        }
+    }
+    private Boolean connectCheck(CustomEmitter emitter){
+        try {
+            emitter.getSseEmitter().send(SseEmitter.event().data("try connect"));
+            return true;
+        }catch (IOException e){
+            return false;
+        }
     }
 
 
