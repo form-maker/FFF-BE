e9fab8f1f97f91cf42a32b3d226b7c963f0635e7
faulty337 <faulty337@gmail.com>
2023-01-31 16:33:36 +0900

    Refactor : 평균 소숫점 수정

--- src/main/java/com/formmaker/fff/stats/StatsMethod.java
+++ src/main/java/com/formmaker/fff/stats/StatsMethod.java
@@ -201,7 +201,8 @@ public class StatsMethod {
         // 평균을 구하는 로직
         int participant = valueList.size();
         int sum = valueList.stream().mapToInt(Integer::intValue).sum();
-        float avg = participant!=0?Math.round(sum / (participant)):0;
+//        float avg = participant!=0?Math.round(sum / (participant)):0;
+        float avg = participant!=0?(Math.round((float)sum / participant*10f)/10f):0;
         List<Float> selectRate = new ArrayList<>();
 
         for (int i = 1; i <= 5; i++) {
