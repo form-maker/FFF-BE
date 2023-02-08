fed5ce1bb0f276f74609d269fb6adb79f372c2ac
faulty337 <faulty337@gmail.com>
2023-02-08 23:48:50 +0900

    Refactor : 쿼리 수정

--- src/main/java/com/formmaker/fff/reply/repository/ReplyRepository.java
+++ src/main/java/com/formmaker/fff/reply/repository/ReplyRepository.java
@@ -2,7 +2,11 @@ package com.formmaker.fff.reply.repository;
 
 import com.formmaker.fff.participant.Participant;
 import com.formmaker.fff.reply.entity.Reply;
+import com.formmaker.fff.survey.entity.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,4 +15,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     List<Reply> findAllByQuestionIdAndCreatedAtAfterAndCreatedAtBefore(Long questionId, LocalDateTime start, LocalDateTime end);
 
     List<Reply> findAllByParticipant(Participant participant);
+
+    @Query(value = "select * from fff.reply r join fff.participant as p on r.participant_id = p.id where p.survey_id = :surveyId", nativeQuery = true)
+    List<Reply> findBySurveyId(@Param("surveyId") Long surveyId);
 }
